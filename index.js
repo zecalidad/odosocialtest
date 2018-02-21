6eeae7a9ebcaf723817cb09fee7afd23e8996930
David <odosocial@orange.fr>
2018-02-21 12:32:36 +0000

    HTML complet

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {"Content-Type": "text/html"});
-    res.end('<p>Voici un paragraphe <strong>HTML</strong> !</p>');
+        res.write('<!DOCTYPE html>'+
+'<html>'+
+'    <head>'+
+'        <meta charset="utf-8" />'+
+'        <title>Ma page Node.js !</title>'+
+'    </head>'+ 
+'    <body>'+
+'     	<p>Voici un paragraphe <strong>HTML</strong> !</p>'+
+'    </body>'+
+'</html>');
+    res.end();
 });
 
 server.listen(process.env.PORT || 3000);
