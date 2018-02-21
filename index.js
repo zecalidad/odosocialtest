c1105b09f70623058b310b306bfa323f52731d7e
David <odosocial@orange.fr>
2018-02-21 12:38:11 +0000

    URL

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+//Type de serveur créé
 var http = require('http');
+//Récupération de l'URL demandée par l'utilisateur
+var url = require('url');
 
 var server = http.createServer(function(req, res) {
+    // Ecriture dans la log de l'url appelée
+    var page = url.parse(req.url).pathname;
+    console.log(page);
+    
     res.writeHead(200, {"Content-Type": "text/html"});
-        res.write('<!DOCTYPE html>'+
+    res.write('<!DOCTYPE html>'+
 '<html>'+
 '    <head>'+
 '        <meta charset="utf-8" />'+
