c495327c8f7542edae7cf3b8b36cdc543ba543a0
David <odosocial@orange.fr>
2018-02-21 08:51:59 +0000

    Entete HTML

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var http = require('http');
 
 var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Salut tout le monde !');
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.end('<p>Voici un paragraphe <strong>HTML</strong> !</p>');
 });
 
 server.listen(process.env.PORT || 3000);
