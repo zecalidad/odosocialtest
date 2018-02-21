051874f33e8553053205896d5ac2bd38c514315d
David <odosocial@orange.fr>
2018-02-21 08:28:02 +0000

    app / server

--- index.js
+++ index.js
@@ -4,6 +4,4 @@ var server = http.createServer(function(req, res) {
   res.writeHead(200);
   res.end('Salut tout le monde !');
 });
-
-var port_number = server.listen(process.env.PORT || 3000);
-server.listen(port_number);
+server.listen(process.env.PORT || 3000;
