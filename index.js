536824f7523e9c72bc57b1e05a5fb784b849bfde
David <odosocial@orange.fr>
2018-02-21 12:46:41 +0000

    Chaines evenement/message

--- index.js
+++ index.js
@@ -6,19 +6,17 @@ var url = require('url');
 var server = http.createServer(function(req, res) {
     // Ecriture dans la log de l'url appelée
     var page = url.parse(req.url).pathname;
-    console.log('URL appelee : '+ page);
+    console.log('URL appelee : ' + page);
     
     res.writeHead(200, {"Content-Type": "text/html"});
-    res.write('<!DOCTYPE html>'+
-'<html>'+
-'    <head>'+
-'        <meta charset="utf-8" />'+
-'        <title>Ma page Node.js !</title>'+
-'    </head>'+ 
-'    <body>'+
-'     	<p>Voici un paragraphe <strong>HTML</strong> !</p>'+
-'    </body>'+
-'</html>');
+    res.write('<!DOCTYPE html>'+ '<html>'+ '    <head>'+ '        <meta charset="utf-8" />'+ '        <title>OdoSocialTest</title>'+ '    </head>'+ '    <body>');
+    if (page=='evenement')
+        res.write('<p>Page <strong>Evenement</strong> !</p>');
+    else if (page=='message')
+        res.write('<p>Page <strong>Message</strong> !</p>');
+    else 
+        res.write('<p>Page <strong>inconnue</strong> !</p>');
+    res.write('</body>'+'</html>');
     res.end();
 });
 
