bad5169103569530462bc98ee585a3d3b194d89c
David <odosocial@orange.fr>
2018-02-21 08:29:21 +0000

    -

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ var server = http.createServer(function(req, res) {
   res.writeHead(200);
   res.end('Salut tout le monde !');
 });
-server.listen(process.env.PORT || 3000;
+
+server.listen(process.env.PORT || 3000);
