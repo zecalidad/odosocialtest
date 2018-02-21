fbc26b56f758dc853281797c9bd0286e5893ce47
David <odosocial@orange.fr>
2018-02-21 12:51:49 +0000

    ajout de / devant la page

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ var server = http.createServer(function(req, res) {
     console.log(scriptexecute+' : DEBUT');
     // Ecriture dans la log de l'url appelée
     var page = url.parse(req.url).pathname;
-    console.log('URL appelee : ' + page);
+    console.log(scriptexecute+' : URL appelee : ' + page);
     res.writeHead(200, {"Content-Type": "text/html"});
     res.write('<!DOCTYPE html>'+ '<html>'+ '    <head>'+ '        <meta charset="utf-8" />'+ '        <title>OdoSocialTest</title>'+ '    </head>'+ '    <body>');
-    if (page=='evenement')
+    if (page=='/evenement')
         res.write('<p>Page <strong>Evenement</strong> !</p>');
-    else if (page=='message')
+    else if (page=='/message')
         res.write('<p>Page <strong>Message</strong> !</p>');
     else 
         res.write('<p>Page <strong>inconnue</strong> !</p>');
