e5568efc7633d13d357d109ed8661180311cf742
David <odosocial@orange.fr>
2018-02-22 07:34:24 +0000

    Test acces répertoire style app.use(express.static('style'));

--- index-express.js
+++ index-express.js
@@ -1,6 +1,9 @@
 var express = require('express');
 
 var app = express();
+// Accès aux répertoires style et images
+app.use(express.static('style'));
+app.use(express.static('images'));
 
 app.get('/test/:idevenement', function(req, res) {
     res.render('test.ejs', {idevenement: req.params.idevenement});
